59b2a638e0ee83a170b708b126b2b13d0267085e
despanad <despanad@student.42barcelona.com>
2024-06-17 19:31:11 +0200

    update libft.h

--- libft.h
+++ libft.h
@@ -6,14 +6,41 @@
 /*   By: despanad <despanad@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 20:47:13 by despanad          #+#    #+#             */
-/*   Updated: 2024/06/10 21:23:15 by despanad         ###   ########.fr       */
+/*   Updated: 2024/06/17 19:30:42 by despanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(char c);
-int	ft_isdigit(char c);
+#import <unistd.h>
+#import <stdlib.h>
+#import <fcntl.h>
+#import <stddef.h>
+
+char	*ft_strdup(char *str);
+char	*ft_strrchr(char *str, int c);
+char	*ft_strnstr(char *str, char *to_find, size_t len);
+size_t	ft_strlen(char *str);
+char	*str_join(char *s1, char *s2);
+void	*memove(void *dest, void *src, size_t n);
+int	ft_isascii(int param);
+char	*ft_substr(char *s, size_t start, size_t len);
+void	*ft_calloc(size_t nmemb, size_t size);
+int	ft_isalpha(int param);
+int	ft_isprint(int param);
+int	ft_atoi(char *str);
+char	*ft_tolower(char *str);
+int	ft_strncmp(char *s1, char *s2, size_t n);
+char	*ft_toupper(char *str);
+int	ft_memcmp(void *s1, void *s2, size_t n);
+char	*ft_strchr(char *str, int c);
+int	ft_isdigit(int param);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
+size_t	ft_strlcat(char *dest, char *src, size_t nb);
+void	ft_bzero(void *s, int n);
+int	ft_isalnum(char *str);
+void	*ft_memcpy(void *dest, void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
