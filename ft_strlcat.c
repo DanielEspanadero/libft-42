0b4d1e6e1704b835e7086d973acd98edebe483d5
despanad <despanad@student.42barcelona.com>
2024-06-18 20:45:20 +0200

    ft_strlcat finished

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,3 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: despanad <despanad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 20:07:48 by despanad          #+#    #+#             */
+/*   Updated: 2024/06/18 20:43:56 by despanad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #include "libft.h"
 
+size_t	ft_strlcat(char *dst, char *src, size_t dst_size)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	if(dst_size == 0)
+		return (src_len);
+	if(dst_size < dst_len)
+		return (src_len += dst_len);
+	else
+		src_len += dst_len;
+	while (src[i] != '\0' && dst_len < dst_size -1 && dst != src)
+	{
+		dst[dst_len] = src[i];
+		i++;
+		dst_len++;
+	}
+	dst[dst_len] = '\0';
+	return (src_len);
+}
