23fad0d669a74f6d4dfa6dfcc997d3f9e683c11d
Daniel Españadero <developer@daniel-espanadero.com>
2024-06-25 22:26:53 +0200

    ft_calloc finished and ft_strdup field created

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: despanad <despanad@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 22:22:51 by despanad          #+#    #+#             */
+/*   Updated: 2024/06/25 22:26:10 by despanad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*calloc(size_t nmemb, size_t size)
+{
+	void *ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, size * nmemb);
+	return (ptr);
+}
