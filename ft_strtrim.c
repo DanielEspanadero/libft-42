2a522f5527990c3bb73cc162c3d597ecbbd0cd56
Daniel Españadero fernández <dif@MacBook-Pro-de-Daniel.local>
2024-06-28 15:27:59 +0200

    strtrim finished

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,8 +6,30 @@
 /*   By: despanad <despanad@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 13:28:52 by despanad          #+#    #+#             */
-/*   Updated: 2024/06/28 13:29:14 by despanad         ###   ########.fr       */
+/*   Updated: 2024/06/28 14:01:14 by despanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strtrim(char *s1, char *set)
+{
+	size_t	strat;
+	size_t	end;
+	char	*str;
+
+	str = 0;
+	if (s1 != 0 && set != 0)
+	{
+		strat = 0;
+		end = ft_strlen(s1);
+		while (s1[strat] && ft_strchr(set, s1[strat]))
+			strat++;
+		while (s1[end - 1] && ft_strchr(set, s1[end - 1] && end > strat))
+			end--;
+		str = (char *)malloc(sizeof(char) * (end - strat + 1));
+		if (str)
+			ft_strlcpy(str, &s1[strat], end - strat + 1);
+	}
+	return (str);
+}
