ac5294d2ca49a2d4e21a16aab383728215a2bfaf
Daniel Españadero <developer@daniel-espanadero.com>
2024-06-29 20:23:45 +0200

    norminette

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: despanad <despanad@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 22:22:51 by despanad          #+#    #+#             */
-/*   Updated: 2024/06/25 22:26:10 by despanad         ###   ########.fr       */
+/*   Updated: 2024/06/29 20:20:53 by despanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*calloc(size_t nmemb, size_t size)
 {
-	void *ptr;
+	void	*ptr;
 
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: despanad <despanad@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 22:28:53 by despanad          #+#    #+#             */
-/*   Updated: 2024/06/25 22:32:52 by despanad         ###   ########.fr       */
+/*   Updated: 2024/06/29 20:21:08 by despanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*strdup(char *s)
 
 	size = ft_strlen(s);
 	ptr = malloc(sizeof(char) * (size + 1));
-	if(ptr == NULL)
+	if (ptr == NULL)
 		return (NULL);
 	ft_memcpy(ptr, s, size);
 	ptr[size] = '\0';
