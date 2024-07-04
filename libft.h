4f5113ea11821376028796de8ed9d0fd0130381b
Daniel Españadero <developer@daniel-espanadero.com>
2024-07-04 22:12:17 +0200

    Update libft

--- libft.h
+++ libft.h
@@ -6,17 +6,17 @@
 /*   By: despanad <despanad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 19:54:51 by despanad          #+#    #+#             */
-/*   Updated: 2024/06/28 22:44:50 by despanad         ###   ########.fr       */
+/*   Updated: 2024/07/04 22:11:25 by despanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#import <unistd.h>
-#import <stdlib.h>
-#import <fcntl.h>
-#import <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
 
 char	*ft_strdup(char *str);
 char	*ft_strrchr(char *str, int c);
@@ -24,22 +24,22 @@ char	*ft_strnstr(char *str, char *to_find, size_t len);
 size_t	ft_strlen(char *str);
 char	*str_join(char *s1, char *s2);
 void	*memove(void *dest, void *src, size_t n);
-int	ft_isascii(int param);
+int		ft_isascii(int param);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 void	*ft_calloc(size_t nmemb, size_t size);
-int	ft_isalpha(int param);
-int	ft_isprint(int param);
-int	ft_atoi(char *nptr);
-int	ft_tolower(int str);
-int	ft_strncmp(char *s1, char *s2, size_t n);
-int	ft_toupper(int str);
-int	ft_memcmp(void *s1, void *s2, size_t n);
+int		ft_isalpha(int param);
+int		ft_isprint(int param);
+int		ft_atoi(char *nptr);
+int		ft_tolower(int str);
+int		ft_strncmp(char *s1, char *s2, size_t n);
+int		ft_toupper(int str);
+int		ft_memcmp(void *s1, void *s2, size_t n);
 char	*ft_strchr(char *str, int c);
-int	ft_isdigit(int param);
+int		ft_isdigit(int param);
 size_t	ft_strlcpy(char *dest, char *src, size_t size);
 size_t	ft_strlcat(char *dest, char *src, size_t nb);
 void	ft_bzero(void *s, int n);
-int	ft_isalnum(char *str);
+int		ft_isalnum(char *str);
 void	*ft_memcpy(void *dest, void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_putchar_fd(char c, int fd);
