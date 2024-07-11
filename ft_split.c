2c95de2c6ec953b9e0a6a5be28b9692aae1a152d
Daniel Españadero <developer@daniel-espanadero.com>
2024-07-11 22:06:50 +0200

    Split finished another time

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: despanad <despanad@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 20:20:06 by despanad          #+#    #+#             */
-/*   Updated: 2024/07/10 20:37:34 by despanad         ###   ########.fr       */
+/*   Updated: 2024/07/11 22:06:29 by despanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,60 @@ static void	ft_free(char **memory, size_t aux)
 	free(memory);
 }
 
+static void split_str(char **ptr, char *str, char c, char nwords)
+{
+	int		i;
+	char	aux;
+
+	i = 0;
+	if ((nwords > 0) && *str)
+	{
+		while (i < nwords - 1)
+		{
+			aux = ft_strchr(str, c);
+			ptr[i] = ft_substr(str, 0 aux - str);
+			if (ptr[i] == NULL)
+			{
+				ft_free(ptr, i);
+				return;
+			}
+			i++;
+			while (*aux == c)
+				aux++;
+			str = aux;
+		}
+		ptr[i] = ft_strdup(str);
+		if (ptr[i] == NULL)
+		{
+			ft_free(ptr, i);
+			return;
+		}
+		ptr[i + 1] = NULL;
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	int	nwords;
+	char	**ptr;
+	char	*str;
+	char	a[2];
+
+	a[0] = c;
+	a[1] = '\0';
+
+	if (s)
+	{
+		str = ft_strtrim(s, a);
+		if (str)
+		{
+			nwords = count_words(str, c);
+			ptr = (char **)calloc((nwords + 1), sizeof(char *));
+			if (ptr)
+				split_str(ptr, str, c, nwords);
+			free(str);
+			return ptr;
+		}
+	}
+	return NULL;
 }
