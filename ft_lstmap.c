b7d06ac371c8c868a89ca6ed33b0e4e101d551a6
Daniel Españadero <developer@daniel-espanadero.com>
2024-07-13 00:21:48 +0200

    lstmap finished

---
+++ ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: despanad <despanad@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/13 00:09:08 by despanad          #+#    #+#             */
+/*   Updated: 2024/07/13 00:15:44 by despanad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*result;
+	t_list	*temp;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	result = NULL;
+	while (lst)
+	{
+		temp = ft_lstnew((*f)(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&result, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&result, temp);
+		temp = temp->next;
+		lst = lst->next;
+	}
+	return (result);
+}
