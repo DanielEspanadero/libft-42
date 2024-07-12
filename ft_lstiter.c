e017399f980180fa9586ebec9b849de5e78b55ca
Daniel Españadero <developer@daniel-espanadero.com>
2024-07-13 00:08:08 +0200

    lstclear and lstiter finished

---
+++ ft_lstiter.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: despanad <despanad@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 22:00:39 by despanad          #+#    #+#             */
+/*   Updated: 2024/07/12 22:04:27 by despanad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (lst == NULL || f == NULL)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
